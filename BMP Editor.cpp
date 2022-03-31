fbd523f09ec6a5110c436cfa9cb374ff6a43d429
kha1edsa1ah <102770185+kha1edsa1ah@users.noreply.github.com>
2022-04-01 01:59:28 +0300

    Update BMP Editor.cpp

--- BMP Editor.cpp
+++ BMP Editor.cpp
@@ -1,3 +1,12 @@
+// FCI – Programming 1 – 2022 - Assignment 3 
+// Program Name: xxxxxx.cpp
+// Last Modification Date: xx/xx/xxxx
+// Author1 and ID and Group: xxxxx xxxxx
+// Author2 and ID and Group: xxxxx xxxxx
+// Author3 and ID and Group: xxxxx xxxxx
+// Teaching Assistant: xxxxx xxxxx
+// Purpose:..........
+
 #include <iostream>
 #include <cstring>
 #include "bmplib.cpp"
