37c3bfa2963e1e51982b43329eb190556df09e81
AyaAli <101468593+Ayaali22@users.noreply.github.com>
2022-04-01 01:04:55 +0200

    Update BMP Editor.cpp

--- BMP Editor.cpp
+++ BMP Editor.cpp
@@ -1,7 +1,7 @@
 // FCI – Programming 1 – 2022 - Assignment 3 
 // Program Name: xxxxxx.cpp
 // Last Modification Date: xx/xx/xxxx
-// Author1 and ID and Group: xxxxx xxxxx
+// Author1 and ID and Group: Aya Ali Hassan &20210083&A
 // Author2 and ID and Group: xxxxx xxxxx
 // Author3 and ID and Group: xxxxx xxxxx
 // Teaching Assistant: xxxxx xxxxx
