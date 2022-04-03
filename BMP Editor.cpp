f8553aaf067a667fe7d98a5a1d0ae5dec3450e21
khaledsa1ah <khsalah555@gmail.com>
2022-04-03 17:37:28 +0300

    I added edge detecting.

--- BMP Editor.cpp
+++ BMP Editor.cpp
@@ -266,4 +266,26 @@ void shrink_image() {
 
 void detect_edges() {
 
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (image[i][j] > 127)
+                image[i][j] = 255;
+            else
+                image[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            new_image[i][j] = (image[i - 1][j - 1] * 0) + (image[i][j - 1] * 1) + (image[i + 1][j - 1] * 0) +
+                              (image[i - 1][j] * 1) + (image[i][j] * -4) + (image[i + 1][j] * 1) +
+                              (image[i - 1][j + 1] * 0) + (image[i][j + 1] * 1) + (image[i + 1][j + 1] * 0);
+        }
+    }
+
+    for (auto &i: new_image) {
+        for (unsigned char &j: i) {
+            j = 255 - j;
+        }
+    }
+    save_new_Image();
 }
